0022ab5534709b29292ad4dd407682a9606ea42c
Eldin Kecap <eldinkecap@gmail.com>
2023-09-21 21:30:15 +0200

    added a form for adding questions previous commit was about deleting sets!!!

--- src/pages/AdminPanelAddQuestion.jsx
+++ src/pages/AdminPanelAddQuestion.jsx
@@ -1,18 +1,47 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { Button, Stack, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import IconButtonWithLabel from "../components/IconButtonWithLabel";
+import { Add } from "@mui/icons-material";
 
 export default function AdminPanelAddQuestion() {
-  const { set } = useParams();
-
+  const { set, language } = useParams();
+  const [showAddQuestionForm, setShowAddQuestionForm] = useState(false);
   return (
-    <Typography
-      variant="h2"
-      textAlign={"center"}
-      color={"white"}
-      sx={{ mt: 10, fontFamily: "Staatliches" }}
-    >
-      {set}
-    </Typography>
+    <>
+      <Typography
+        variant="h2"
+        textAlign={"center"}
+        color={"white"}
+        sx={{ mt: 10, fontFamily: "Staatliches" }}
+      >
+        {language}
+      </Typography>
+      <Typography
+        variant="h5"
+        textAlign={"center"}
+        color={"white"}
+        sx={{ fontFamily: "Staatliches", color: "text.secondary" }}
+      >
+        {set}
+      </Typography>
+      {showAddQuestionForm ? (
+        <Stack alignItems={"center"} gap={2} mt={2}>
+          <TextField placeholder="Question" />
+          <TextField placeholder="Answer" />
+          <Button variant="contained" className="gradientButton">
+            Submit
+          </Button>
+        </Stack>
+      ) : (
+        <IconButtonWithLabel
+          label={"Add Question"}
+          onClickHandler={() => {
+            setShowAddQuestionForm((curr) => true);
+          }}
+          icon={<Add />}
+        />
+      )}
+    </>
   );
 }
