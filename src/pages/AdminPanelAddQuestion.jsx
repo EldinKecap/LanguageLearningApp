7e8b808ed8547eaeb8019b44b3803004cf6e3be3
Eldin Kecap <eldinkecap@gmail.com>
2023-09-23 12:49:41 +0200

    started implementig a reducer function for error handling

--- src/pages/AdminPanelAddQuestion.jsx
+++ src/pages/AdminPanelAddQuestion.jsx
@@ -1,12 +1,19 @@
 import { Button, Stack, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import { useParams } from "react-router-dom";
 import IconButtonWithLabel from "../components/IconButtonWithLabel";
 import { Add } from "@mui/icons-material";
 
+function addQuestionFormReducer(state, action) {}
+
 export default function AdminPanelAddQuestion() {
   const { set, language } = useParams();
   const [showAddQuestionForm, setShowAddQuestionForm] = useState(false);
+  const [errorState, dispatch] = useReducer(first, {
+    questionError: false,
+    answerError: false,
+  });
+
   return (
     <>
       <Typography
@@ -29,7 +36,11 @@ export default function AdminPanelAddQuestion() {
         <Stack alignItems={"center"} gap={2} mt={2}>
           <TextField placeholder="Question" />
           <TextField placeholder="Answer" />
-          <Button variant="contained" className="gradientButton">
+          <Button
+            variant="contained"
+            sx={{ fontFamily: "Staatliches" }}
+            className="gradientButton"
+          >
             Submit
           </Button>
         </Stack>
