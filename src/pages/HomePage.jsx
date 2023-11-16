20f1c5de32cad3c56de9aa06a2753fefbf7f60ab
Eldin Kecap <eldinkecap@gmail.com>
2023-11-16 23:09:39 +0100

    added some text to the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-import HeroSection from '../components/HeroSection'
-import InformationSection from '../components/InformationSection'
+import React from "react";
+import NavBar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
+import InformationSection from "../components/InformationSection";
+import { Box, Button, Stack, Typography } from "@mui/material";
 
 export default function HomePage() {
-    return (
-        <>
-            <HeroSection />
-            <InformationSection />
-        </>
-    )
+  return (
+    <>
+      <HeroSection />
+      <InformationSection />
+      <Stack direction={"row"} width={"80%"} justifyContent={"end"} alignItems={"center"} px={6}>
+        <Box>
+          <Button variant="contained" size="large" className="gradientButton">
+            Register
+          </Button>
+        </Box>
+        <Box >
+          <Typography
+            variant="h1"
+            sx={{
+              fontFamily: "Staatliches",
+              minWidth: "200px",
+              pt: 3,
+              textAlign: "right",
+            }}
+            color={"text.primary"}
+          >
+            Try for <br />
+            <strong className="gradientLetters">Free</strong>!
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              fontFamily: "Staatliches",
+              minWidth: "200px",
+              float:"right"
+            }}
+            color={"text.secondary"}
+            width={"50%"}
+          >
+            For all new participants we offer a free first set in any language
+            of their choosing.
+          </Typography>
+        </Box>
+      </Stack>
+    </>
+  );
 }
