d847d1f0540f5e787c6c4033d61d4bc4468ef45d
EldinKecap <76916396+EldinKecap@users.noreply.github.com>
2023-11-22 00:26:00 +0100

    added TryForFreeSection component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,72 +2,16 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import InformationSection from "../components/InformationSection";
-import { Box, Button, Stack, Typography, useMediaQuery } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+
+import TryForFreeSection from "../components/TryForFreeSection";
 
 export default function HomePage() {
-  const navigator = useNavigate();
-  const mobile = useMediaQuery("(max-width:800px)");
+ 
   return (
     <>
       <HeroSection />
       <InformationSection />
-      <Stack
-        direction={mobile ? "column-reverse" : "row"}
-        m={"auto"}
-        width={"80%"}
-        justifyContent={"space-around"}
-        alignItems={"center"}
-        gap={3}
-        px={6}
-        pb={2}
-        >
-        <Box>
-          <Button variant="contained" size="large" className="gradientButton" sx={{
-            fontFamily: "Staatliches",
-            fontSize: "30px",
-            borderRadius: "50px",
-            borderColor: "transparent",
-            px: "35px",
-          }}
-            color="success"
-            onClick={() => {
-              navigator('/login')
-            }}
-          >
-            Register
-          </Button>
-        </Box>
-        <Box sx={{
-          minWidth: "200px",
-          width: "50%"
-        }}>
-          <Typography
-            variant="h1"
-            sx={{
-              fontFamily: "Staatliches",
-              pt: 3,
-              textAlign: mobile ? "center" : "right",
-            }}
-            color={"text.primary"}
-          >
-            Try for <br />
-            <strong className="movingGradientBackgroundLetters">Free</strong>!
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{
-              fontFamily: "Staatliches",
-              float: "right",
-              textAlign:mobile ? "center" :"initial"
-            }}
-            color={"text.secondary"}
-          >
-            For all new participants we offer a free first set in any language
-            of their choosing.
-          </Typography>
-        </Box>
-      </Stack>
+      <TryForFreeSection />
     </>
   );
 }
