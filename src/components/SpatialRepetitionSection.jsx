76da575e8c6e6dc9274958b8f16140e1bcd4da61
Eldin Kecap <eldinkecap@gmail.com>
2023-11-30 22:34:22 +0100

    added description to the spatial repetition section

--- src/components/SpatialRepetitionSection.jsx
+++ src/components/SpatialRepetitionSection.jsx
@@ -14,6 +14,18 @@ export default function SpatialRepetitionSection() {
       >
         Spatial Repetition
       </Typography>
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: "Staatliches",
+          color: "text.secondary",
+          textAlign: "center",
+        }}
+      >
+        Spaced repetition is a memory technique that involves reviewing and
+        recalling information at optimal spacing intervals until the information
+        is learned at a high level.
+      </Typography>
     </Box>
   );
 }
